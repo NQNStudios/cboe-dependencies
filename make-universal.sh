c7cde17a4f07db6aade5e86e9fa8a220296a136a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-07 09:13:04 -0500

    try mac universal

---
+++ make-universal.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ -z "$CONFIGURATION" ]; then
+    CONFIGURATION=Release
+fi
+
+INTEL=dependencies-macos-intel-$CONFIGURATION
+SILICON=dependencies-macos-silicon-$CONFIGURATION
+
+cp -r $SILICON/include ./
+mkdir lib
+
+combine() {
+    lipo -create $SILICON/lib/$1 $INTEL/lib/$1 -output lib/$1
+}
+
+combine_all() {
+    for LIB in $LIBS; do
+        (cd ../../ && combine $LIB)
+    done
+}
+
+(cd $SILICON/lib && LIBS=*.a combine_all)
+(cd $SILICON/lib && LIBS=*.dylib combine_all)
