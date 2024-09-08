9a23bb9213e67d0546f82a13a8b5c00c1497765f
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-07 20:43:47 -0500

    pass configuration to zlib.build.bat

--- .github/workflows/scripts/win/zlib-build.bat
+++ .github/workflows/scripts/win/zlib-build.bat
@@ -2,6 +2,6 @@
 setlocal enabledelayedexpansion
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
-	"%%i" -clp:ForceConsoleColor -target:Build -property:Configuration=Release -property:Platform=%1 "zlib/build/zlib.sln"
+	"%%i" -clp:ForceConsoleColor -target:Build -property:Configuration=%2 -property:Platform=%1 "zlib/build/zlib.sln"
 	exit /b !errorlevel!
 )
